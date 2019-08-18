1927189d96784181acd2417ff1c1648b520d5567
ayamiya <kinkaioba@gmail.com>
2019-08-18 08:16:47 +0000

    add kadaicolumn

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
+    
     if @task.save
       flash[:success] = 'タスクが投稿されました'
       redirect_to @task
@@ -24,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'タスクが編集されました'
@@ -40,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'タスクが削除されました'
@@ -52,6 +50,11 @@ end
 
   private
 
+def set_task
+  @task = Task.find(params[:id])
+end
+
+
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
